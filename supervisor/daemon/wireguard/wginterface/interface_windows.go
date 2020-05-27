133493b1c38854f2f11a86976dd2fb398ae16bcf
anjmao <andzej.maciusovic@gmail.com>
2020-05-27 21:34:46 +0300

    Use custom win wg ipc listener

--- supervisor/daemon/wireguard/wginterface/interface_windows.go
+++ supervisor/daemon/wireguard/wginterface/interface_windows.go
@@ -31,23 +31,9 @@ import (
 	"github.com/songgao/water"
 	"golang.org/x/sys/windows"
 	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/ipc"
+	"golang.zx2c4.com/wireguard/ipc/winpipe"
 	"golang.zx2c4.com/wireguard/tun"
 )
-
-func init() {
-	sddl := "O:SYD:P(A;;GA;;;SY)"
-	socketGroup := "Users"
-	sid, err := winio.LookupSidByName(socketGroup)
-	if err != nil {
-		panic(err)
-	}
-	sddl += fmt.Sprintf("(A;;GRGW;;;%s)", sid)
-	ipc.UAPISecurityDescriptor, err = windows.SecurityDescriptorFromString(sddl)
-	if err != nil {
-		panic(err)
-	}
-}
 
 // New creates new WgInterface instance.
 func New(interfaceName string, uid string, subnet net.IPNet) (*WgInterface, error) {
@@ -104,7 +90,7 @@ func New(interfaceName string, uid string, subnet net.IPNet) (*WgInterface, erro
 	}, logger)
 
 	log.Println("Setting interface configuration")
-	uapi, err := ipc.UAPIListen(interfaceName)
+	uapi, err := UAPIListen(interfaceName)
 	if err != nil {
 		return nil, fmt.Errorf("could not listen for user API wg configuration: %w", err)
 	}
@@ -221,4 +207,73 @@ func NewLogger(level int, prepend string, output io.Writer) *device.Logger {
 		log.Ldate|log.Ltime,
 	)
 	return logger
+}
+
+func UAPIListen(name string) (net.Listener, error) {
+	sddl := "O:SYD:P(A;;GA;;;SY)"
+	socketGroup := "Users"
+	sid, err := winio.LookupSidByName(socketGroup)
+	if err != nil {
+		panic(err)
+	}
+	sddl += fmt.Sprintf("(A;;GRGW;;;%s)", sid)
+	securityDescriptor, err := windows.SecurityDescriptorFromString(sddl)
+	if err != nil {
+		return nil, err
+	}
+
+	config := winpipe.PipeConfig{
+		SecurityDescriptor: securityDescriptor,
+	}
+	listener, err := winpipe.ListenPipe(`\\.\pipe\ProtectedPrefix\Administrators\WireGuard\`+name, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	uapi := &UAPIListener{
+		listener: listener,
+		connNew:  make(chan net.Conn, 1),
+		connErr:  make(chan error, 1),
+	}
+
+	go func(l *UAPIListener) {
+		for {
+			conn, err := l.listener.Accept()
+			if err != nil {
+				l.connErr <- err
+				break
+			}
+			l.connNew <- conn
+		}
+	}(uapi)
+
+	return uapi, nil
+}
+
+type UAPIListener struct {
+	listener net.Listener // unix socket listener
+	connNew  chan net.Conn
+	connErr  chan error
+	kqueueFd int
+	keventFd int
+}
+
+func (l *UAPIListener) Accept() (net.Conn, error) {
+	for {
+		select {
+		case conn := <-l.connNew:
+			return conn, nil
+
+		case err := <-l.connErr:
+			return nil, err
+		}
+	}
+}
+
+func (l *UAPIListener) Close() error {
+	return l.listener.Close()
+}
+
+func (l *UAPIListener) Addr() net.Addr {
+	return l.listener.Addr()
 }
